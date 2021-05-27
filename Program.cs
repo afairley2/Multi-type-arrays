4e8b736394f88954cb0ee75f1ea50fdf19c1178f
afairley2 <55838836+afairley2@users.noreply.github.com>
2021-05-27 01:53:40 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multi_dimentional_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] names = { { "fred", "willma" }, { "blogs", "smith" },{ "doctor", "mr" } };
+            Console.WriteLine("{0} {1} {2}", names[2, 0], names[0, 1], names[1, 1]);
+        }
+    }
+}
